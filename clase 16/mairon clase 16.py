3d0f3d3e2722b88960b33edb6c87f6b48e1cde49
AlejandroDS <ducuarasatizabalalejandro@gmail.com>
2025-05-31 17:02:51 -0500

    nueva clase

--- clase 16/mairon clase 16.py
+++ clase 16/mairon clase 16.py
@@ -1,6 +1,6 @@
 #Controladores de flujo (condicionales)(if)
 #if  y else
-print("If y Else")
+"""print("If y Else")
 edad=30
 if edad>=18:
     print("Es adulto")
@@ -15,8 +15,21 @@ if x>10:
     if x>20:
         print("y tambien por encima de 20!")
     else:
-        print("Pero no por enciama de 20")
+        print("Pero no por enciama de 20")"""
 
+#sentencia elif
+print("Elif")
+c=int(input("Ingrese un numero: "))
+b=int(input("ingrese otro numero: "))
+a=b*c
+if a==4:
+    print("A es igual a 4")
+elif a==5:
+    print("A es igual a 5")
+elif a==6:
+    print("A es igual a 6")
+else:
+    print("No se cumple la condicion")
 
 
 
