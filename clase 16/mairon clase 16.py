a146a44f89ea6063f0773834d8f80eaae3c99548
AlejandroDS <ducuarasatizabalalejandro@gmail.com>
2025-05-31 16:55:52 -0500

    nueva clase

---
+++ clase 16/mairon clase 16.py
@@ -0,0 +1,24 @@
+#Controladores de flujo (condicionales)(if)
+#if  y else
+print("If y Else")
+edad=30
+if edad>=18:
+    print("Es adulto")
+else:
+    print("No se cumple la condicion")
+
+#multiples if
+print("Multiples if\nEjemplo 1:")
+x=25
+if x>10:
+    print("Por encima de 10")
+    if x>20:
+        print("y tambien por encima de 20!")
+    else:
+        print("Pero no por enciama de 20")
+
+
+
+
+
+#
