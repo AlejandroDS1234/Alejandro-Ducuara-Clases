702483c8b9315616565f58534c177320fb45eff6
AlejandroDS <ducuarasatizabalalejandro@gmail.com>
2025-06-01 17:18:36 -0500

    nuevo proyecto personal (contador de tareas)

--- ramdom/contador de tareas.py
+++ ramdom/contador de tareas.py
@@ -1,5 +1,5 @@
 #contar cuantas tareas ha hecho
-print("Contador De Tareas")
+"""print("Contador De Tareas")
 tareas=int(input("Ingrese la cantidad de tareas que tiene: "))
 while True:
     boton=input("Si completo una tarea presione enter")
@@ -8,6 +8,34 @@ while True:
         print("Ya no le quedan mas tareas")
         break
     else:
-        print(f"le quedan {tareas} tareas")
-    
+        print(f"le quedan {tareas} tareas")"""
+
+
+
+#tareas mas avanzado
+print("Ingrese el nombre de la asignatura en que tiene tareas: ")
+materias=[]
+numeros=[0]
+while True:
+    tarea=input("(deje en blanco si no tiene mas tareas) ")
+    materias.append(tarea)
+    numeros.append(numeros[-1]+1)
+    if tarea=="":
+        materias.remove("")
+        for numero in numeros:
+            cantidad=len(materias)-numero
+            if cantidad==0:
+                print(f"ha finalizado todas las tareas")
+                input("presione enter para salir")
+                materias.clear()
+                numeros.clear()
+                numeros.append(0)
+                print("Ingrese el nombre de la asignatura en que tiene tareas: ")
+                break
+            else:
+                print(f"le quedan {cantidad} tareas, siga con la tarea de {materias[numero]}")
+                input("Presione enter para la siguiente")
+
+            
+
 
