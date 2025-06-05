1752b9ef077c895faf93b8e3aec144d714fd1e1a
AlejandroDS <ducuarasatizabalalejandro@gmail.com>
2025-06-05 16:33:36 -0500

    clase 18 taller

---
+++ clase 18/mairon clase 18 -taller-.py
@@ -0,0 +1,72 @@
+#Taller condicionales
+#Operaciones matematicas
+#1
+"""print("1) Numero positivo negativo o cero")
+anum=int(input("Ingrese un numero: "))
+if anum>0:
+    print(f"El numero {anum} es positivo\n")
+elif anum<0:
+    print(f"El numero {anum} es negativo\n")
+else:
+    print("Su numero es 0\n")
+
+#2
+print("2) Calcular el mayor")
+bnum1=int(input("Ingrese el primer numero: "))
+bnum2=int(input("Ingrese un segundo numero: "))
+if bnum1>bnum2:
+    print(f"El numero {bnum1} es el mayor\n")
+elif bnum1<bnum2:
+    print(f"El numero {bnum2} es el mayor\n")
+else:
+    print("Los numeros son iguales\n")
+
+#3
+print("3) Par o Impar")
+cnum=int(input("Ingrese un numero: "))
+if round(cnum/2, 2)==cnum//2+0.00:
+    print(f"El numero {cnum} es par\n")
+else:
+    print(f"El numero {cnum} es impar\n")
+        
+#4
+print("4) El numero esta entre 10 y 20?")
+dnum=int(input("Ingrese un numero: "))
+if dnum>10 and dnum<20:
+    print(f"El numero {dnum} esta entre 10 y 20\n")
+else:
+    print(f"El numero {dnum} no esta en el rango de 10 y 20\n")
+
+#5
+print("5) Encontrar el numero mayor")
+enum1=int(input("Ingrese el primer numero: "))
+enum2=int(input("Ingrese el segundo numero: "))
+enum3=int(input("Ingrese el tercero numero: "))
+if enum1>enum2 and enum1>enum3:
+    print(f"El numero mayor es: {enum1}\n")
+elif enum2>enum1 and enum2>enum3:
+    print(f"El numero mayor es: {enum2}\n")
+elif enum3>enum1 and enum3>enum2:
+    print(f"El numero mayor es: {enum3}\n")
+else:
+    print(f"Los numeros nos iguales\n")"""
+
+#6
+print("6) precio final")
+fnum=float(input("Ingrese el precio final: "))
+if fnum>100:
+    fop=fnum-((fnum*10)/100)
+    print(f"Se le aplico un descuento del 10% por superar los 100$, su total es de: {fop}\n")
+else:
+    print(f"El precio a pagar es {fnum}\n")
+
+#7
+
+    
+
+
+
+    
+
+
+    
