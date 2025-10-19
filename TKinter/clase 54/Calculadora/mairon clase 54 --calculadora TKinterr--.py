7a531514ccd406c78d71e1725e1adbeab63371ad
AlejandroDS <ducuarasatizabalalejandro@gmail.com>
2025-10-19 17:28:05 -0500

    pagina de calculadoras

--- TKinter/clase 54/Calculadora/mairon clase 54 --calculadora TKinterr--.py
+++ TKinter/clase 54/Calculadora/mairon clase 54 --calculadora TKinterr--.py
@@ -1,27 +1,34 @@
 import tkinter as tk
 ventana=tk.Tk()
-operacion=tk.Label(ventana, text="", background="blue", width=20, justify="right")
-operacion.grid(row=0, column=0, columnspan=4)
-espacio=tk.Label(ventana, text="",)
-espacio.grid(row=1, column=0, columnspan=4)
-resul=False
+ventana.geometry("165x257")
+ventana.config(background="gray32")
+calculadora=tk.Frame(ventana, background="gray32")
+calculadora.grid(row=0, column=0, pady=5)
+teclado=tk.Frame(calculadora, background="gray32")
+teclado.grid(row=2, column=0, padx=3, pady=10)
+operacion=tk.Label(calculadora, text="", background="SteelBlue1", width=20, height=-2 ,anchor="e", highlightthickness=5, highlightcolor="dark slate gray", highlightbackground="dark slate gray")
+operacion.grid(row=0, column=0, columnspan=4, sticky="s")
+
+
+estado={"resul":False}
+
 
 botones=[
-    ('7', 2, 2), ('8', 2, 1), ('9', 2, 0), ('/', 2, 3),
-    ('4', 3, 2), ('5', 3, 1), ('6', 3, 0), ('*', 3, 3),
-    ('1', 4, 2), ('2', 4, 1), ('3', 4, 0), ('-', 4, 3),
-    ('0', 5, 1), ('.', 5, 2), ('+', 5, 0)]
+    ('7', 3, 2), ('8', 3, 1), ('9', 3, 0), ('/', 2, 3),
+    ('4', 4, 2), ('5', 4, 1), ('6', 4, 0), ('*', 2, 2),
+    ('1', 5, 2), ('2', 5, 1), ('3', 5, 0), ('-', 2, 1),
+    ('0', 6, 1), ('.', 6, 0), ('+', 2, 0)]
 
 for boton in botones:
-    tk.Button(ventana, text=boton[0], width=5, height=2, command=lambda num=boton[0]: escribir(num)).grid(row=boton[1], column=boton[2])
+    tk.Button(teclado, text=boton[0], width=4, height=2,background="gray64",command=lambda num=boton[0]: escribir(num)).grid(row=boton[1], column=boton[2], padx=1)
 
 def escribir (num):
-    if resul and not operacion["text"].strip().isdigit():
-        operacion["text"]=""
-  
-   
+    global resul
+    
+    if estado["resul"] and  (True!=(eval(operacion["text"])==int or eval(operacion["text"])==float)):
+        borrarall()
         
-    if operacion["text"].strip() !="":
+    if operacion["text"] !="":
         if operacion["text"][len(operacion["text"])-1] in "+-*/.":
             if num.isdigit():
                 operacion["text"]+=str(num)
@@ -30,25 +37,34 @@ def escribir (num):
     else:
         if num.isdigit():
             operacion["text"]+=str(num)
-            
-    global resul
-    resul=False
+    
+    estado["resul"]=False
+    
+    
             
 def borrar():
     operacion["text"]=operacion["text"][0:-1]    
 
 def igual():
     if operacion["text"].strip() !="":
-        if operacion["text"][-1] in "+-*/.":
-            operacion["text"]=operacion["text"][0:-1]
-            operacion["text"]=str(eval(operacion["text"]))
-        else:
-            operacion["text"]=str(eval(operacion["text"]))
-        resul=True
+        try:
+            if operacion["text"][-1] in "+-*/.":
+                operacion["text"]=operacion["text"][0:-1]
+                operacion["text"]=str(eval(operacion["text"]))
+            else:
+                operacion["text"]=str(eval(operacion["text"]))
+        except:
+            operacion["text"]="Error"
+        estado["resul"]=True
+
+def borrarall():
+    operacion["text"]=""
             
             
-botonigual=tk.Button(ventana, text="=", width=5, height=2, command=igual)
-botonigual.grid(row=5, column=3)
-botonmen=tk.Button(ventana, text="<--", width=5, height=2, command=borrar)
-botonmen.grid(row=5, column=0)
+botonigual=tk.Button(teclado, text="=", width=4, height=2,background="gray64", command=igual)
+botonigual.grid(row=6, column=2)
+botonmen=tk.Button(teclado, text="<--", width=4, height=2,background="gray64" ,command=borrar)
+botonmen.grid(row=4, column=3)
+botonmen=tk.Button(teclado, text="AC", width=4, height=2,background="gray64" ,command=borrarall)
+botonmen.grid(row=3, column=3)
 ventana.mainloop()
--- ramdom/calculadora2.0/sever.py
+++ ramdom/calculadora2.0/sever.py
@@ -6,5 +6,14 @@ cal=Flask(__name__)
 def index():
     return render_template("index.html")
 
+@cal.route('/calculadora')
+def calculadora():
+    return render_template('calculadora.html')
+
+@cal.route('/calculadora1')
+def calculadora1():
+    return render_template('calculadora1.html')
+
+
 if __name__=="__main__":
     cal.run(debug=True)
